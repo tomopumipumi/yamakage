196e295994522dbf0f355774c2d76197a1a54612
tomopumipumi <tomo.pumi.pumi@gmail.com>
2026-08-11 19:38:54 +0900

    微調整

--- web-site/src/features/share/components/ShareImageCard.tsx
+++ web-site/src/features/share/components/ShareImageCard.tsx
@@ -10,6 +10,7 @@ import iconUrl from '../../../assets/icon.svg';
 import type { SkylineChartPoint } from '../../calculator/hooks/useSkylineData';
 import type { MapLayerOption } from '../../map/store/mapStore';
 
+
 const stripHtml = (html: string) => {
   const tmp = document.createElement('div');
   tmp.innerHTML = html;
